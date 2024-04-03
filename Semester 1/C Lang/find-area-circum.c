96247653ac2d85fa0c84c142d35e25c14b9a3c18
Arth Jaiswal <ansharvindjaiswal@gmaiol.com>
2024-04-04 02:34:56 +0530

    Reccursion based Circumference program

--- Semester 1/C Lang/find-area-circum.c
+++ Semester 1/C Lang/find-area-circum.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-#include <conio.h>
 
-void main(){
-    float r, area, circum;
-    clrscr();
+void calculateCircleProperties(float r, float *area, float *circum, float *dia) {
+    if (r < 0) {
+        printf("Radius cannot be negative.\n");
+        return;
+    }
+
+    *area = 3.14 * r * r;
+    *circum = 2 * 3.14 * r;
+    *dia = 2 * r;
+}
+
+int main() {
+    float r, area, circum, dia;
     printf("Enter the radius of the circle: ");
-    scanf("%f", &r);    
-    area = 3.14 * r * r;
-    circum = 2 * 3.14 * r;
+    scanf("%f", &r);
+    
+    calculateCircleProperties(r, &area, &circum, &dia);
+
     printf("Area of the circle = %f\n", area);
     printf("Circumference of the circle = %f\n", circum);
-    getch();
+    printf("Diameter of the circle = %f\n", dia);
+
+    return 0;
 }
